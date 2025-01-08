b9b38ff13d60ed7f61586aaa810e7327dbc1a93f
Manku Kumar Pathak <manku.pathak@delta.exchange>
2025-01-08 11:49:28 +0530

    db connection isssue fixed

--- src/database/engine/iam_engine.py
+++ src/database/engine/iam_engine.py
@@ -2,24 +2,14 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 class IamEngine:
-    __engine = None
-    __session = None
-
-    @staticmethod
-    def __ensure_loaded():
-        if IamEngine.__engine is None:
-            username = os.getenv('IAM_RDS_USERNAME')
-            password = os.getenv('IAM_RDS_PASSWORD')
-            host = os.getenv('IAM_RDS_HOSTNAME')
-            port = os.getenv('IAM_RDS_PORT')
-            database = os.getenv('IAM_RDS_DB_NAME')
-            IamEngine.__engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}')
-            Session = sessionmaker(bind=IamEngine.__engine)
-            IamEngine.__session = Session()
-
     @staticmethod
     def get_session():
-        IamEngine.__ensure_loaded()
-        return IamEngine.__session
+        username = os.getenv('IAM_RDS_USERNAME')
+        password = os.getenv('IAM_RDS_PASSWORD')
+        host = os.getenv('IAM_RDS_HOSTNAME')
+        port = os.getenv('IAM_RDS_PORT')
+        database = os.getenv('IAM_RDS_DB_NAME')
+        engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}')
+        Session = sessionmaker(bind=engine)
+        return Session()
--- src/database/engine/timescale_engine.py
+++ src/database/engine/timescale_engine.py
@@ -4,22 +4,13 @@ from sqlalchemy.orm import sessionmaker
 
 
 class TimescaleEngine:
-    __engine = None
-    __session = None
-
-    @staticmethod
-    def __ensure_loaded():
-        if TimescaleEngine.__engine is None:
-            username = os.getenv('TIMESCALE_USERNAME')
-            password = os.getenv('TIMESCALE_PASSWORD')
-            host = os.getenv('TIMESCALE_HOSTNAME')
-            port = os.getenv('TIMESCALE_PORT')
-            database = os.getenv('TIMESCALE_DB_NAME')
-            TimescaleEngine.__engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}')
-            Session = sessionmaker(bind=TimescaleEngine.__engine)
-            TimescaleEngine.__session = Session()
-
     @staticmethod
     def get_session():
-        TimescaleEngine.__ensure_loaded()
-        return TimescaleEngine.__session
+        username = os.getenv('TIMESCALE_USERNAME')
+        password = os.getenv('TIMESCALE_PASSWORD')
+        host = os.getenv('TIMESCALE_HOSTNAME')
+        port = os.getenv('TIMESCALE_PORT')
+        database = os.getenv('TIMESCALE_DB_NAME')
+        engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}')
+        Session = sessionmaker(bind=engine)
+        return Session()
