17b77d2650bc9ac11ac0575998dbca6887f25a8f
Manku Kumar Pathak <manku.pathak@delta.exchange>
2025-05-06 11:47:11 +0530

    mysql lib upgradde and slack ratelimited error handling

--- src/vendor/slack.py
+++ src/vendor/slack.py
@@ -7,7 +7,6 @@ import time
 class SlackNotifier:
     @staticmethod
     def send_alert(message, retry_count=3):
-        success = False
         try:
             webhook_url = os.getenv("SLACK_WEBHOOK_URL")
             channel = os.getenv("SLACK_CHANNEL")
@@ -19,13 +18,12 @@ class SlackNotifier:
             )
             if response.status_code == 200:
                 logger.info("Slack message sent successfully")
-                success = True
+            elif response.status_code == 429:
+                retry_after = int(response.headers.get("Retry-After", 2))
+                logger.error(f"Rate limit exceeded. Retrying after {retry_after} seconds.")
+                time.sleep(retry_after)
+                SlackNotifier.send_alert(message, retry_count - 1) if retry_count > 0 else None
             else:
                 logger.error(f"Failed to send slack message. Status code: {response.status_code}, Response: {response.text}")
         except Exception as e:
             logger.error(f"An error occurred while sending slack alert: {e}")
-        finally:
-            if not success and retry_count > 0:
-                time.sleep(2)
-                logger.info(f"Retrying to send slack alert. Remaining retries: {retry_count - 1}")
-                SlackNotifier.send_alert(message, retry_count - 1) if retry_count > 0 else None
